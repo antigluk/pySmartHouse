77eb82e1e69ae6c73aa2210b888f853d80937aa0
Roman Rader <antigluk@gmail.com>
2012-12-05 18:04:12 +0200

    saving to file

--- watch.py
+++ watch.py
@@ -3,15 +3,17 @@ from time import sleep
 
 WATCH_LIST = ["k8temp", ]
 
-def main():
+def api_sensors():
     sensors.init()
     try:
         while True:
             for sensor in sensors.iter_detected_chips():
                 if sensor.prefix in WATCH_LIST:
+                    wfile = file("/tmp/smarthouse/sensor_%s" % sensor.prefix, "w")
                     #MONGO?
                     for feature in sensor:
                         print "%s: %s=%s" % (sensor.prefix, feature.name, feature.get_value())
+                        print>>wfile, "%s=%s" % (feature.name, feature.get_value())
 
             sleep(2)
     finally:
@@ -19,4 +21,4 @@ def main():
         
 
 if __name__ == '__main__':
-    main()
+    api_sensors()
