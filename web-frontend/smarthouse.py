91f3fbe0a012d8a223ad7ddcc1e4bfe995246f68
Roman Rader <antigluk@gmail.com>
2012-12-06 19:59:35 +0200

    accordeon

--- web-frontend/smarthouse.py
+++ web-frontend/smarthouse.py
@@ -9,12 +9,23 @@ from watchd import watch, LOG_PATH, LOG_PATH_FORMAT
 import sh
 import os
 import fnmatch
+import socket
+import fcntl
+import struct
 
 app = Flask(__name__)
 
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
 def get_info():
     return {"host": sh.hostname().strip(),
-            "ip": sh.hostname("-i").strip(),
+            "ip": get_ip_address("eth0"),#sh.hostname("-i").strip(),
             "version": "0.0.1",
             }
 
@@ -27,7 +38,7 @@ def index():
         file_name = os.path.join(LOG_PATH, sensor)
         if os.path.exists(file_name):
             data = file(file_name).readlines()
-            sensors.append({"name":sensor, "data":[line.strip().split("=") for line in data]})
+            sensors.append({"name":sensor[len("sensor_"):], "data":[line.strip().split("=") for line in data]})
 
     return render_template("index.html", data=sensors, info=get_info())
 
