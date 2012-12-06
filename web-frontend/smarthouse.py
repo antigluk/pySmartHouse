d3f014be75e645366961eb44671cfe3b3db84907
Roman Rader <antigluk@gmail.com>
2012-12-07 01:08:07 +0200

    base template

--- web-frontend/smarthouse.py
+++ web-frontend/smarthouse.py
@@ -24,13 +24,23 @@ def get_ip_address(ifname):
     )[20:24])
 
 def get_info():
+    try:
+        ip = get_ip_address("eth0")
+    except IOError:
+        try:
+            ip = get_ip_address("wlan0")
+        except IOError:
+            try:
+                ip = get_ip_address("lo")
+            except IOError:
+                ip = "0.0.0.0"
     return {"host": sh.hostname().strip(),
-            "ip": get_ip_address("eth0"),#sh.hostname("-i").strip(),
+            "ip": ip,#sh.hostname("-i").strip(),
             "version": "0.0.1",
             }
 
-@app.route("/")
-def index():
+@app.route("/all")
+def all_sensors():
     sensors = []
     for sensor in os.listdir(LOG_PATH):
         if not fnmatch.fnmatch(sensor, 'sensor_*'):
@@ -40,7 +50,22 @@ def index():
             data = file(file_name).readlines()
             sensors.append({"name":sensor[len("sensor_"):], "data":[line.strip().split("=") for line in data]})
 
+    return render_template("all_sensors.html", data=sensors, info=get_info())
+
+
+@app.route("/")
+def index():
+    sensors = {}
+    for sensor in os.listdir(LOG_PATH):
+        if not fnmatch.fnmatch(sensor, 'sensor_*'):
+            continue
+        file_name = os.path.join(LOG_PATH, sensor)
+        if os.path.exists(file_name):
+            data = file(file_name).readlines()
+            sensors.update({sensor[len("sensor_"):]: dict([line.strip().split("=") for line in data])})
+
     return render_template("index.html", data=sensors, info=get_info())
+
 
 if __name__ == "__main__":
     app.run(debug = False, host='0.0.0.0')
