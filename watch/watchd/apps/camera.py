e0a4c44794ceb35988beef41ceb8ca799b710ac6
Roman Rader <antigluk@gmail.com>
2013-03-17 22:45:19 +0200

    save moving

--- watch/watchd/apps/camera.py
+++ watch/watchd/apps/camera.py
@@ -1,4 +1,4 @@
-from time import sleep
+from time import sleep, time
 import os
 import datetime
 
@@ -9,6 +9,7 @@ from watchd import sensor
 DEVICE = '/dev/video0'
 IMG_PATH = '/home/aiko/pysmarthouse/resources/static/'
 IMG_LAST = '/home/aiko/pysmarthouse/resources/static/lastimg.dat'
+IMG_DB = '/home/aiko/diff/'
 
 
 @sensor
@@ -16,6 +17,10 @@ def photo():
     """
     Take photo
     """
+    RECORDING = False
+    last_treshold = 0
+
+    sh.mkdir('-p', IMG_DB)
     while True:
         if not os.path.exists(DEVICE):
             sleep(5)
@@ -29,5 +34,22 @@ def photo():
             # filename = "%s.jpg" % (
             #     datetime.datetime.isoformat(datetime.datetime.now()))
             filename = "lastimg.jpg"
+            sh.mv(filename, filename+".old")
             sh.mv('00000001.jpg', filename)
             print >>file(IMG_LAST, 'w'), filename
+
+            if RECORDING:
+                filename_d = "%s.jpg" % (
+                             datetime.datetime.isoformat(datetime.datetime.now()))
+                sh.cp(filename, IMG_DB + filename_d)
+
+                if time() - last_treshold > 10:
+                    RECORDING = False
+
+            k = sh.compare('-metric', 'AE', '-fuzz', '5%', 'lastimg.jpg',
+                           'lastimg.jpg.old')
+            if k > 2000:
+                RECORDING = True
+                last_treshold = time()
+
+            sleep(1)
