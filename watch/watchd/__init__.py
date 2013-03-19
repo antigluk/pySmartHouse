8e6794189df25a1bbdb8d2870869f33ec17adbc3
Roman Rader <antigluk@gmail.com>
2013-03-19 23:23:36 +0200

    log watch

--- watch/watchd/__init__.py
+++ watch/watchd/__init__.py
@@ -1,5 +1,7 @@
-
 "ACPI sensors list"
+import sys
+
+
 WATCH_LIST = ["k8temp", "coretemp", "via686a", "msp430"]
 LOG_PATH = "/tmp/smarthouse"
 LOG_PATH_FORMAT = "%s/sensor_%%s" % LOG_PATH
@@ -9,6 +11,8 @@ FUNC_PREFIX = "sensors_watch_process_"
 
 def sensor(f):
     def _sens():
+        sys.stdout = open(str(f.func_name) + ".log", "a+")
         f()
+
     _sens.func_name = FUNC_PREFIX + f.func_name
     return _sens
